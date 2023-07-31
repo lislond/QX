b947b7321b31d2aea6ee72bc31ce0565be8e94b6
lislond <91541636+lislond@users.noreply.github.com>
2023-07-31 10:06:18 +0800

    Create days_ad.js

---
+++ Scripts/days_ad.js
@@ -0,0 +1,20 @@
+/***********************************************
+
+> 应用名称：Days Matter (倒数日) 去广告脚本
+> 解锁说明：可能需要清除缓存
+> 更新时间：2023-6-20
+> 特别说明：⚠️⚠️⚠️
+          本脚本仅供学习交流使用，禁止转载、售卖
+          ⚠️⚠️⚠️
+
+[rewrite_local]
+
+https:\/\/idays-cdn\.2q10\.com\/app\/idays\/app_ad url reject
+https:\/\/app-cdn\.2q10\.com\/app\/idays\/honored url reject
+https:\/\/app-cdn\.2q10\.com\/app\/idays\/popups url reject-dict
+
+[mitm]
+
+hostname = idays-cdn.2q10.com,app-cdn.2q10.com
+
+***********************************************/
